f3480b40c32b1cf3e32eb7284b5a6d1077e94fbe
Filip Drzazga <filip.drzazga@gmail.com>
2025-03-06 23:59:19 +0000

    Updates

---
+++ images/images.jsx
@@ -0,0 +1,40 @@
+import BIO_MOBILE_XS from "./bio_mobile_img_xs.webp";
+import BIO_MOBILE_M from "./bio_mobile_img_m.webp";
+import BIO_MOBILE_LG from "./bio_mobile_img_lg.webp";
+import BIO_MOBILE_XL from "./bio_mobile_img_xl.webp";
+import BIO_TABLET_SM from "./bio_tablet_img_sm.webp";
+import BIO_TABLET_LG from "./bio_tablet_img_lg.webp";
+import BIO_SCREEN_XS from "./bio_screen_img_xs.webp";
+import BIO_SCREEN_M from "./bio_screen_img_m.webp";
+import BIO_SCREEN_LG from "./bio_screen_img_lg.webp";
+
+import CONTACT_MOBILE_XS from "./contact_mobile_img_xs.webp";
+import CONTACT_MOBILE_M from "./contact_mobile_img_m.webp";
+import CONTACT_MOBILE_LG from "./contact_mobile_img_lg.webp";
+import CONTACT_MOBILE_XL from "./contact_mobile_img_xl.webp";
+import CONTACT_TABLET_SM from "./contact_tablet_img_sm.webp";
+import CONTACT_TABLET_LG from "./contact_tablet_img_lg.webp";
+import CONTACT_SCREEN_XS from "./contact_screen_img_xs.webp";
+import CONTACT_SCREEN_M from "./contact_screen_img_m.webp";
+import CONTACT_SCREEN_LG from "./contact_screen_img_lg.webp";
+
+export {
+  BIO_MOBILE_XS,
+  BIO_MOBILE_M,
+  BIO_MOBILE_LG,
+  BIO_MOBILE_XL,
+  BIO_TABLET_SM,
+  BIO_TABLET_LG,
+  BIO_SCREEN_XS,
+  BIO_SCREEN_M,
+  BIO_SCREEN_LG,
+  CONTACT_MOBILE_XS,
+  CONTACT_MOBILE_M,
+  CONTACT_MOBILE_LG,
+  CONTACT_MOBILE_XL,
+  CONTACT_TABLET_SM,
+  CONTACT_TABLET_LG,
+  CONTACT_SCREEN_XS,
+  CONTACT_SCREEN_M,
+  CONTACT_SCREEN_LG,
+};
