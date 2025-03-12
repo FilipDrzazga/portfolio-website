1662eda0b2a60d85943b20fd68b179f6534242b5
Filip Drzazga <filip.drzazga@gmail.com>
2025-03-12 22:18:51 +0000

    rename routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import { Routes, Route } from "react-router";
 import BioPage from "./pages/BioPage";
 import PlaygroundPage from "./pages/PlaygroundPage";
 
-
 const App = () => {
   return (
-      <Routes>
-          <Route path='/bio' element={<BioPage />} />
-          <Route path="/playground" element={<PlaygroundPage />} />
-      </Routes>
+    <Routes>
+      <Route path="/bio" element={<BioPage />} />
+      <Route path="/playground" element={<PlaygroundPage />} />
+    </Routes>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/portfolio-website/bio">
+    <BrowserRouter basename="/portfolio-website">
       <App />
     </BrowserRouter>
   </StrictMode>
